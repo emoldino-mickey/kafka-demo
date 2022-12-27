4a35ddfc35861392f7ac21b0b1eea901ca6e19cd
emoldino-mickey <mickey.park@emoldino.com>
2022-12-27 14:17:01 +0900

    Update

--- src/main/java/com/emoldino/kafka/config/KafkaConfig.java
+++ src/main/java/com/emoldino/kafka/config/KafkaConfig.java
@@ -1,18 +1,34 @@
 package com.emoldino.kafka.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.retrytopic.RetryTopicConfigurationBuilder;
 
 @Configuration
-public class KafkaTopicConfig {
+public class KafkaConfig {
 	
+	@Value(value = "${spring.kafka.bootstrap-servers}")
+    private String kafkaAddress;
+		
 	@Value(value = "${mms.topic.name}")
 	private String mmsTopic;
 
+	
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> cfgs = new HashMap<>();
+        cfgs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaAddress);
+        return new KafkaAdmin(cfgs);
+    }
+	
 	/** If you want to create a new topic for use with the MMS, add it below. **/
 	@Bean
 	public NewTopic mmsTopic() {
@@ -21,4 +37,6 @@ public class KafkaTopicConfig {
 				.replicas(1) //				
 				.build();
 	}	
+	
+	
 }
